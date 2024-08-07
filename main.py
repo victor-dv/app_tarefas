bb361f12d500daaca3d90a4ce2a160e77bf47a9b
victor-dv <js813417@gmail.com>
2024-08-06 21:01:35 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import tkinter as tk 
+from tkinter import ttk, font, messagebox
+from tkinter import PhotoImage
+
+# Criando janela {
+janela = tk.Tk()
+janela.title("APP DE TAREFAS") #-->  configurando o titulo da janela
+janela.configure(bg="#F0F0F0") #--> defindo uma cor padrão para o fundo da janela
+janela.geometry("500x600") #-->Passando as dimensões da janela 
+
+cabecalho = font.Font(family="Garamond", size= 24, weight="bold") #--> Estou especificando a font do titulo
+cabecalho = tk.Label(janela, text= "APP DE TAREFAS", font=cabecalho, bg="#F0F0F0", fg= "#333").pack(pady=20)#--> passando as especificações do que sera escrito 
+
+frame = tk.Frame(janela, bg="#F0F0F0") #--> passando as especificaçoes do local onde armazena o adionarTarefa e o botaoAdiciona
+frame.pack(pady=10) 
+
+adicionaTarefas = tk.Entry(frame, font=("Garamond", 14), relief=tk.FLAT, bg = "white", fg="grey", width=30)
+adicionaTarefas.pack(side=tk.LEFT, padx=10) #--> Local onde o usuario passa a tarefa
+
+botaoAdiciona = tk.Button(frame, text="Adicionar Tarefa", bg="#038C3E", fg="white", height=1, width=15, font=("Roboto", 11), relief=tk.FLAT)
+botaoAdiciona.pack(side=tk.LEFT, padx=10) #--> botão utilizado para gerar uma tarefa
+
+#frama para lista de tarefas, com scroll
+frameListaTarefas = tk.Frame(janela, bg="white")
+frameListaTarefas.pack(fill=tk.BOTH, expand=True, padx=10, pady = 10) 
+
+canvas = tk.Canvas(frameListaTarefas, bg="White")
+canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)#--> primeiras especificações da tela onde aparecera as tarefas
+
+
+scroollBar = ttk.Scrollbar(frameListaTarefas, orient="vertical", command=canvas.yview)
+scroollBar.pack(side=tk.RIGHT, fill=tk.Y) # --> Gerando um scrool para navegação nas tarefas
+canvas.configure(yscrollcommand= scroollBar.set) 
+canvasInterior = tk.Frame(canvas, bg="white")
+canvas.create_window((0,0), window=canvasInterior, anchor="nw")
+canvasInterior.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all" )) )
+
+janela.mainloop()
